af3d4c71061cda6f36c130474d76a23438cc8941
MickeyKing832 <mickeyking852@gmail.com>
2020-11-14 15:23:49 +0800

    updated on 14/11/2020/15:23

--- main.py
+++ main.py
@@ -1,74 +1,121 @@
 import tkinter as tk
 from PIL import Image
-import json, os
+import csv, os
 
-p1 = None
-p2 = None
-selected_area = None
-question = ''
-test_img = ''
-default_width, default_height = 842, 595
-counter = 0
-data = {}
-info = {}
-data['result'] = []
-data['img_info'] = []
+TOP_L_X: int = 0
+TOP_L_Y: int = 0
+BOT_R_X: int = 0
+BOT_R_Y: int = 0
 
-def image_file_load():
-    global width, height, img_files, data
+SELECTED_AREA = {}
+SELECTED_AREA['rectangle'] = []
+
+INFO = {}
+INFO['img_info'] = []
+INFO['result'] = []
+
+QUESTION: str = ''
+TEST_IMG: object = ''
+
+WIDTH: int = 842
+HEIGHT: int = 595
+COUNTER: int = 0
+
+
+def image_file_load() -> None:
+    global WIDTH, HEIGHT
 
     default_path = '/root/Pictures/Turing_Test/'
-    unavaiable_fomat = ['.jpg', 'tif', 'tiff', '.bmp', '.jpeg', '.gif']
+    convert_need_formmat = ['.jpg', 'tif', 'tiff', '.bmp', '.jpeg', '.gif']
+    try:
+        for r, d, f in os.walk(default_path):
 
-    for r, d, f in os.walk(default_path):
-        for file in f:
-            for extension in unavaiable_fomat:
-                if extension in file.lower():
+            for file in f:
+                for extension in convert_need_formmat:
+                    if extension in file.lower():
+                        im = Image.open(default_path + file)
+                        im.save(default_path + file.lower().replace(extension, '.png'))
+                        os.remove(default_path + file)
+
+        for r, d, f in os.walk(default_path):
+            for file in f:
+                if '.png' in file:
                     im = Image.open(default_path + file)
-                    im.save(default_path + file.lower().replace(extension, '.png'))
-                    os.system('rm ' + default_path + file)
+                    width, height = im.size
 
-    for r, d, f in os.walk(default_path):
-        for file in f:
-            if '.png' in file:
-                im = Image.open(default_path + file)
-                width, height = im.size
-
-                if (width, height) != (default_width, default_height):
-                    im = im.resize((default_width, default_height))
+                if (width, height) != (WIDTH, HEIGHT):
+                    im = im.resize((WIDTH, HEIGHT))
                     os.system('rm ' + default_path + file)
                     im.save(default_path + file)
-                data['img_info'].append({
+                INFO['img_info'].append({
                     'path': default_path + file,
                     'image name': file
                 })
 
+    except Exception as e:
 
-def convas_setup():
-    global test_img, data, counter
-    test_img = tk.PhotoImage(file=data['img_info'][counter]['path'])
-    counter += 1
+        alert = tk.Tk()
+
+        alert.title('Error!!!')
+        alert.geometry('600x400')
+        error_label = tk.Label(alert, text=f'{e}').pack()
+        exit_b = tk.Button(alert, text='close', command=lambda: alert.destroy()).pack(side=tk.BOTTOM)
+
+        alert.mainloop()
 
 
-def summit():
-    global p1, p2, data, counter
+def convas_setup() -> None:
+    global TEST_IMG, INFO, COUNTER
 
-    default_path = '/root/Documents/Github/Turing-Test-GUI/Turing-Test-GUI/Test-Result.json'
+    TEST_IMG = tk.PhotoImage(file=INFO['img_info'][COUNTER]['path'])
+    COUNTER += 1
 
+
+def submit() -> None:
+    global TEST_IMG
     try:
-        data['result'].append({
-            'Image-name:': str(data['img_info'][counter]['image name']) + ' ',
-            'vertex-1: ': str(p1) + ' ',
-            'vertex-2:': str(p2)
-        })
+        default_path = '/root/Documents/Github/Turing-Test-GUI/Test-Result.csv'
+        fieldnames = ['image-name', 'rectengle id', 'Top-left-x', 'Top-left-y', 'Bottom-right-x', 'Bottom-right-y']
+        repeated = False
 
-        with open('/root/Documents/Github/Turing-Test-GUI/Turing-Test-GUI/Test-Result.json', 'w') as outfile:
-            json.dump(data['result'], outfile)
-        outfile.close()
+        if os.path.exists(default_path):
+            out_file = open(default_path, 'r')
+            in_file = open(default_path, 'a')
+            reader = csv.DictReader(out_file, fieldnames)
+            writer = csv.DictWriter(in_file, fieldnames)
 
-        Test_area.delete(selected_area)
+            for row in reader:
+                if row['image-name'] == str(INFO['img_info'][COUNTER - 1]['image name']):
+                    for i in INFO['result']:
+                        i = str(i).split(',')
+                        for j in out_file:
+                            # f"{j['Top-left-x']},{j['Top-left-y']},{j['Bottom-right-x']},{j['Bottom-right-y']}"
+                            if f"{i[2]},{i[3]},{i[4]},{i[5]}" is f"{j[2]},{j[3]},{j[4]},{j[5]}":
+                                repeated = True
+                                break
+                        if repeated is not True:
+                            writer.writerow(
+                                {'image-name': i[0], 'rectengle id': i[1], 'Top-left-x': i[2], 'Top-left-y': i[3],
+                                 'Bottom-right-x': i[4], 'Bottom-right-y': i[5]})
+                        # else:
+                        #     raise RuntimeError
+
+            in_file.close()
+            out_file.close()
+        else:
+            in_file = open(default_path, 'w')
+            wirter = csv.DictWriter(in_file, fieldnames)
+            for i in INFO['result']:
+                i = i.split(',')
+                wirter.writerow(
+                    {'image-name': i[0], 'rectengle id': i[1], 'Top-left-x': i[2], 'Top-left-y': i[3],
+                     'Bottom-right-x': i[4], 'Bottom-right-y': i[5]})
+            in_file.close()
+
+        test_area.delete(SELECTED_AREA['rectangle'])
         convas_setup()
-        Test_area.itemconfig(image, image=test_img)
+        test_area.itemconfig(image, image=TEST_IMG)
+        test_area.itemconfig(test_line, fill='blue')
 
     except IndexError:
         popup = tk.Tk()
@@ -76,53 +123,83 @@ def summit():
         label = tk.Label(popup, text='You finished the Turing Test').pack(side=tk.TOP, anchor=tk.CENTER)
         close_b = tk.Button(popup, text='Exit', command=lambda: popup.destroy()).pack(side=tk.BOTTOM, anchor=tk.CENTER)
 
-        # root.destroy()
+        root.destroy()
         popup.mainloop()
 
-
-def mouse_R_click(event):
-    global selected_area
-    if selected_area != None:
-        Test_area.delete(selected_area)
-
-
-def mouse_L_click(event):
-    global p1
-    p1 = event.x, event.y
+    except RuntimeError:
+        test_area.delete(SELECTED_AREA['rectangle'])
+        convas_setup()
+        test_area.itemconfig(image, image=TEST_IMG)
+        print(TEST_IMG)
 
 
-def mouse_move(event):
-    global p1
-    global p2
-    global selected_area
+def reset_canvas() -> None:
+    global SELECTED_AREA
 
-    p2 = event.x, event.y
-    if p1 != None:
-        if p2 != None:
-            if selected_area != None:
-                Test_area.delete(selected_area)
-            selected_area = Test_area.create_rectangle(p1, p2, outline='red', width=3)
+    while True:
+        counter = len(SELECTED_AREA['rectangle'])
+        if counter < 1:
+            pass
+        else:
+            test_area.delete(SELECTED_AREA['rectangle'][counter - 1])
+            SELECTED_AREA['rectangle'].remove(SELECTED_AREA['rectangle'][counter - 1])
+            INFO['img_info'].remove(INFO['img_info'][counter - 1])
+
+        yield None
+
+
+def get_top_left_coordinate(x: int, y: int) -> None:
+    global TOP_L_X, TOP_L_Y
+
+    while True:
+        TOP_L_X = x
+        TOP_L_Y = y
+        SELECTED_AREA['rectangle'].append(
+            test_area.create_rectangle(TOP_L_X, TOP_L_Y, TOP_L_X, TOP_L_Y, outline='red', width=3))
+        yield None
+
+
+def sub_get_top_left_coordinate(event) -> None:
+    run = get_top_left_coordinate(event.x, event.y)
+    next(run)
+
+
+def get_bottom_right_coordinate(event) -> None:
+    global TOP_L_Y, TOP_L_X, BOT_R_X, BOT_R_Y
+
+    counter = len(SELECTED_AREA['rectangle'])
+    BOT_R_X, BOT_R_Y = event.x, event.y
+
+    test_area.coords(SELECTED_AREA['rectangle'][counter - 1], BOT_R_X, BOT_R_Y, TOP_L_X, TOP_L_Y)
+
+
+def sub_get_bottom_right_coordinate(event) -> None:
+    counter = len(SELECTED_AREA['rectangle'])
+    INFO['result'].append(
+        f"{INFO['img_info'][COUNTER - 1]['image name']},R{counter - 1},{TOP_L_X},{TOP_L_Y},{BOT_R_X},{BOT_R_Y}")
 
 
 root = tk.Tk()
 root.title('Turing Test GUI')
-root.geometry(str(default_width + 30) + 'x' + str(default_height + 50))
+window_size: tuple = (WIDTH + 30, HEIGHT + 50)
+root.geometry(str(WIDTH + 30) + 'x' + str(HEIGHT + 50))
 
-question = 'I am question'
-title = tk.Label(root, text='' + question)
+QUESTION = 'I am question'
+title = tk.Label(root, text=QUESTION)
 title.pack(side=tk.TOP)
 
-Test_area = tk.Canvas(root, width=default_width, height=default_height)
-Test_area.bind("<Button-1>", mouse_L_click)
-Test_area.bind('<B1-Motion>', mouse_move)
-Test_area.bind('<Button-3>', mouse_R_click)
-Test_area.pack(side=tk.TOP, anchor=tk.CENTER)
+test_area = tk.Canvas(root, width=WIDTH, height=HEIGHT)
+test_area.bind("<Button-1>", sub_get_top_left_coordinate)
+test_area.bind('<B1-Motion>', get_bottom_right_coordinate)
+test_area.bind('<ButtonRelease-1>', sub_get_bottom_right_coordinate)
+reset = reset_canvas()
+test_area.bind('<Button-3>', lambda value: next(reset))
+test_area.pack(side=tk.TOP, anchor=tk.CENTER)
 
 image_file_load()
 convas_setup()
-image = Test_area.create_image(1, 1, anchor=tk.NW, image=test_img)
-
-ok_button = tk.Button(root, text='OK', command=summit)
+image = test_area.create_image(1, 1, anchor=tk.NW, image=TEST_IMG)
+ok_button = tk.Button(root, text='OK', command=submit)
 ok_button.pack(side=tk.RIGHT, anchor=tk.W)
 
 root.mainloop()
